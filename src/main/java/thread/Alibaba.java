f88aad3ce7f93bdb97ee6be315c902cacacefa01
yangyouxing <youxingyang@annoroad.com>
2018-03-29 12:09:52 +0800

    new class submit

---
+++ src/main/java/thread/Alibaba.java
@@ -0,0 +1,11 @@
+package thread;
+
+/**
+ * Created on 2018/3/29
+ * Author: youxingyang.
+ */
+public class Alibaba {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
