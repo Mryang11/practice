f88f36ecdff0da5e40b1668a53a6fb6cdee400bd
Mryang11 <yyxyyxyang@126.com>
2018-03-30 20:32:38 +0800

    tmp

--- src/main/java/r/RCall.java
+++ src/main/java/r/RCall.java
@@ -12,9 +12,13 @@ public class RCall {
         // 返回与当前 Java 应用程序相关的运行时对象
         Runtime run = Runtime.getRuntime();
         try {
-            // 注意：对字符串中路径\进行转义VolinPlot(filePath, imagePath)
-            //String cmds = "Rscript D:/IDEAWORKPLACE/practice/src/my_script.R tea coff eoo fff";
-            String cmds = "Rscript D:/IDEAWORKPLACE/practice/src/test.r D:/IDEAWORKPLACE/practice/src/QAT16ANAA00018F-test5.txt cx";
+            // 注意：对字符串中路径\进行转义
+            String projectPath = RCall.class.getResource("/").getPath();
+            System.out.println(projectPath);
+            projectPath = projectPath.replace("\\\\", "/");
+            System.out.println(projectPath);
+            String cmds = "Rscript " + projectPath + "my_script.R tea coff eoo fff";
+            //String cmds = "Rscript D:\\IDEAWORKPLACE\\practice\\src\\test.r D:\\IDEAWORKPLACE/practice/src/QAT16ANAA00018F-test5.txt cx";
             // 启动另一个进程来执行命令
             Process p = run.exec(cmds);
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
