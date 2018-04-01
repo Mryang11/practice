32c55e1288aa99ff123d26580886acf1a915d36b
Mryang11 <yyxyyxyang@126.com>
2018-04-01 18:27:31 +0800

    for foreach iterator test

---
+++ src/main/java/fortest/ForThree.java
@@ -0,0 +1,16 @@
+package main.java.fortest;
+
+/**
+ * @Author youxingyang
+ * @date 2018/4/118:26
+ */
+public class ForThree {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/main/java/fortest/ForTwo.java
@@ -0,0 +1,15 @@
+package main.java.fortest;
+
+/**
+ * @Author youxingyang
+ * @date 2018/4/118:26
+ */
+public class ForTwo {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        for (Integer s : arr) {
+            System.out.println(s);
+        }
+    }
+}
